9637ec8c4b76dbb0d14f229ea3e770e74e84d415
tung098 <55541214+tung098@users.noreply.github.com>
2019-12-30 00:42:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo 'Xin chào bạn Tùng';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h2>
+    <a href="https://lovenhi.herokuapp.com/" title="học lập trình online" rel="follow, index">Xin chào bạn Nhi, mời bạn click tại đây---</a></h2>
+</body>
+</html>
